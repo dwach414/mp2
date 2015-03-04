7d7287d39bfdb9bebb5a7ba9be4f8eaf1ebe3011
Daniel Wachtel <dwach414@gmail.com>
2015-03-03 22:06:22 -0600

    first draft remake

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-var divs = document.getElementsByClassName('alert');
-for(var i=0; i<divs.length; i++) {
-  divs[i].addEventListener("click", highlightThis);
-  /*
-  divs[i].addEventListener("click", highlightThis, true);
-  divs[i].addEventListener("click", highlightThis, false);*/
-}
+//var divs = document.getElementsByClassName('alert');
+//for(var i=0; i<divs.length; i++) {
+//  divs[i].addEventListener("click", highlightThis);
+//  /*
+//  divs[i].addEventListener("click", highlightThis, true);
+//  divs[i].addEventListener("click", highlightThis, false);*/
+//}
+//
+//function highlightThis(event) {
+//    //event.stopPropagation();
+//
+//    var backgroundColor = this.style.backgroundColor;
+//    this.style.backgroundColor='yellow';
+//    alert(this.className);
+//    this.style.backgroundColor=backgroundColor;
+//}
 
-function highlightThis(event) {
-    //event.stopPropagation();
-  
-    var backgroundColor = this.style.backgroundColor;
-    this.style.backgroundColor='yellow';
-    alert(this.className);
-    this.style.backgroundColor=backgroundColor;
-}
